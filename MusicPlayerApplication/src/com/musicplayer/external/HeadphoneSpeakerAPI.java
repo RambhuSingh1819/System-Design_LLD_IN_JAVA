4d89223ecdc2735f391ac7eed9c15ac66a4c3aff
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-07 12:26:42 +0530

    Add HeadphoneSpeakerAPI for headphone playback

---
+++ MusicPlayerApplication/src/com/musicplayer/external/HeadphoneSpeakerAPI.java
@@ -0,0 +1,10 @@
+package com.musicplayer.external;
+
+/**
+ * Third-party Headphone SDK simulation.
+ */
+public class HeadphoneSpeakerAPI {
+    public void playSongViaHeadphone(String data) {
+        System.out.println("[Headphone SDK] Playing directly to headphone drivers: " + data);
+    }
+}
