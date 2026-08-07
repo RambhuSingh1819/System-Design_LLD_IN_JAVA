89cc945adf2585b437c0e0e2e70b86f57cd96bfe
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-07 12:27:08 +0530

    Create WiredSpeakerAPI.java

---
+++ MusicPlayerApplication/src/com/musicplayer/external/WiredSpeakerAPI.java
@@ -0,0 +1,10 @@
+package com.musicplayer.external;
+
+/**
+ * Third-party Wired Speaker SDK simulation.
+ */
+public class WiredSpeakerAPI {
+    public void playSongViaWiredSpeaker(String data) {
+        System.out.println("[Wired Speaker SDK] Playing via Wired AUX/USB: " + data);
+    }
+}
