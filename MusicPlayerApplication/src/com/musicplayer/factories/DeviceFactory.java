4e761fde80f1a79aee5128c82cfe562970343afd
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-07 12:19:12 +0530

    Implementaiton of  DeviceFactory

---
+++ MusicPlayerApplication/src/com/musicplayer/factories/DeviceFactory.java
@@ -0,0 +1,24 @@
+package com.musicplayer.factories;
+
+import com.musicplayer.device.*;
+import com.musicplayer.enums.DeviceType;
+
+public class DeviceFactory {
+    
+    public static AudioOutputDevice createDevice(DeviceType dt) {
+        if (dt == null) {
+            throw new IllegalArgumentException("DeviceType cannot be null");
+        }
+        
+        switch (dt) {
+            case BLUETOOTH:
+                return new BluetoothSpeakerAdapter();
+            case WIRED:
+                return new WiredSpeakerAdapter();
+            case HEADPHONE:
+                return new HeadphoneSpeakerAdapter();
+            default:
+                throw new IllegalArgumentException("Unknown DeviceType: " + dt);
+        }
+    }
+}
