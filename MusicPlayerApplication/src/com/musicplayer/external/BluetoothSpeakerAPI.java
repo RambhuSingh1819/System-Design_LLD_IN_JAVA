7023b3bf2dee5da43dee76787e9c53a70b4ca541
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-07 12:26:19 +0530

    Add BluetoothSpeakerAPI for Bluetooth playback

---
+++ MusicPlayerApplication/src/com/musicplayer/external/BluetoothSpeakerAPI.java
@@ -0,0 +1,10 @@
+package com.musicplayer.external;
+
+/**
+ * Third-party Bluetooth Speaker SDK simulation.
+ */
+public class BluetoothSpeakerAPI {
+    public void playSongViaBluetooth(String songTitle) {
+        System.out.println("[Bluetooth Speaker SDK] Playing via Bluetooth: " + songTitle);
+    }
+}
