e13ecd11f1e44aec863575adf9575de774063da4
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-14 20:14:58 +0530

    Implement PaytmBankingSystem class

---
+++ Build Payment Gateway/src/main/java/com/payment/banking/PaytmBankingSystem.java
@@ -0,0 +1,10 @@
+package com.payment.banking;
+
+/**
+ * Paytm specific banking system implementation.
+ */
+public class PaytmBankingSystem extends BaseBankingSystem {
+    public PaytmBankingSystem() {
+        super("PaytmBankingSystem");
+    }
+}
