711f7d7cb02af8fe038ce0628cd285adce1aab8c
Rambhu Singh <156609631+RambhuSingh1819@users.noreply.github.com>
2026-08-14 20:15:35 +0530

    Implement Razorpay banking system class

---
+++ Build Payment Gateway/src/main/java/com/payment/banking/RazorpayBankingSystem.java
@@ -0,0 +1,10 @@
+package com.payment.banking;
+
+/**
+ * Razorpay specific banking system implementation.
+ */
+public class RazorpayBankingSystem extends BaseBankingSystem {
+    public RazorpayBankingSystem() {
+        super("RazorpayBankingSystem");
+    }
+}
